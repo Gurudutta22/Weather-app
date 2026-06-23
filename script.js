c00e86f43a04bdb370d4ecd7695c4efedc21e42d
Gurudutta Pradhan <Guruduttapradhan140@gmail.com>
2026-06-23 17:26:33 +0530

    Made some change

--- script.js
+++ script.js
@@ -1,11 +1,91 @@
 const  apiKey = "5e7db72a8bfe4b186870ea902f74787b";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
 
-async function checkWeather() {
-    const response = await fetch(apiURL + '&appid=${apiKey}')
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+
+
+const searchbox = document.querySelector(".search input");
+
+const searchButton = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+
+async function checkWeather(city) {
+
+const response = await fetch(`${apiURL}${city}&appid=${apiKey}`);
+
+
+
+if (response.status == 404) {
+
+    document.querySelector(".error").style.display = "block";
+
+    document.querySelector(".weather").style.display = "none";
+
+}else {
+
     var data = await response.json();
 
-    console.log(data);
-}
 
-checkWeather();
+
+    console.log(data);
+
+
+
+    document.querySelector(".city").innerHTML = data.name;
+
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+
+
+    if(data.weather[0].main == "Clouds") {
+
+        weatherIcon.src = "images/cloudy.png";
+
+    }else if(data.weather[0].main == "Clear") {
+
+        weatherIcon.src = "images/clear.png";
+
+    }else if(data.weather[0].main == "Rain") {
+
+        weatherIcon.src = "images/rain.png";
+
+    }else if(data.weather[0].main == "Drizzle") {
+
+        weatherIcon.src = "images/drizzle.png";
+
+    }else if(data.weather[0].main == "Mist") {
+
+        weatherIcon.src = "images/mist.png";
+
+    }else if(data.weather[0].main == "Snow") {
+
+        weatherIcon.src = "images/snow.png";
+
+    }else if(data.weather[0].main == "Thunderstorm") {
+
+        weatherIcon.src = "images/thunderstorm.png";
+
+    }
+
+ 
+
+    document.querySelector(".weather").style.display = "block";
+
+    document.querySelector(".error").style.display = "none";
+
+}}
+
+
+
+searchButton.addEventListener("click", () => {
+
+checkWeather(searchbox.value);
+
+}) 
